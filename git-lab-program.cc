9c6bd775c4b2ba6c710dde5550c87c7440421f4a
Helena Hall <hh272720@ohio.edu>
2021-09-02 16:33:34 -0400

    Add git-lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Helena Hall>
+ *        Date: <9-2-21>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
